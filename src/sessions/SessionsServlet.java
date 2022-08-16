607b953f1583e9b0f3cae27744b7f4e054fa384e
yuanyuanlele <1468817353@qq.com>
2022-08-16 18:31:56 +0800

    sessions

---
+++ src/sessions/SessionsServlet.java
@@ -0,0 +1,23 @@
+package sessions;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(value = "/ss")
+public class SessionsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session= req.getSession();
+        System.out.println("sessioneid="+session.getId());
+    }
+}
